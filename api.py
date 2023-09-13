507404768338d5c5e69bc990c266e6303876c56e
theeduke <oscarian528@gmail.com>
2023-09-13 22:43:07 +0300

    person api

---
+++ api.py
@@ -0,0 +1,73 @@
+from flask import Flask, jsonify, request, abort
+from configuration import DATABASE_URI
+import psycopg2
+
+app = Flask(__name__)
+
+#Define Database Connection
+def get_db_connection():
+    conn = psycopg2.connect(DATABASE_URI)
+    return conn
+
+#Create Database Table
+def create_table():
+    conn = get_db_connection()
+    conn.execute('CREATE TABLE IF NOT EXISTS persons (id INTEGER PRIMARY KEY, name TEXT, age INTEGER)')
+    conn.commit()
+    conn.close()
+
+#Create Person
+@app.route('/api', methods=['POST'])
+def create_person():
+    data = request.get_json()
+    if 'name' not in data or 'age' not in data:
+        abort(400) # Bad request
+    name = data['name']
+    age = data['age']
+
+    conn = get_db_connection()
+    conn.execute('INSERT INTO persons (name, age) VALUES (?, ?)', (name, age))
+    conn.commit()
+    conn.close()
+
+    return jsonify({'message': 'Person created successfully!'}), 201
+
+#Read Person
+@app.route('/api/<int:user_id>', methods=['GET'])
+def get_person(user_id):
+    conn = get_db_connection()
+    person = conn.execute('SELECT * FROM persons WHERE id = ?', (user_id,)).fetchone()
+    conn.close()
+
+    if person is None:
+        abort(404) # Not found
+
+    return jsonify({'id': person['id'], 'name': person['name'], 'age': person['age']}), 200
+
+#Update Person
+@app.route('/api/<int:user_id>', methods=['PUT'])
+def update_person(user_id):
+    data = request.get_json()
+    name = data.get('name')
+    age = data.get('age')
+
+    conn = get_db_connection()
+    conn.execute('UPDATE persons SET name = ?, age = ? WHERE id = ?', (name, age, user_id))
+    conn.commit()
+    conn.close()
+
+    return jsonify({'message': 'Person updated successfully!'}), 200
+
+#Delete Person
+@app.route('/api/<int:user_id>', methods=['DELETE'])
+def delete_person(user_id):
+    conn = get_db_connection()
+    conn.execute('DELETE FROM persons WHERE id = ?', (user_id,))
+    conn.commit()
+    conn.close()
+
+    return jsonify({'message': 'Person deleted successfully!'}), 200
+
+if __name__ == '__main__':
+    create_table()
+    app.run(debug=True)
---
+++ configuration.py
@@ -0,0 +1,3 @@
+#DATABASE_URI = "postgresql://username:password@localhost:5432/your_database"
+DATABASE_URI="postgresql://name_api_user:0JsY7xEqSiUmAxKq4wsRDl783auQRqLP@dpg-ck0oemr6fquc738rgnpg-a.oregon-postgres.render.com/name_api"
+
